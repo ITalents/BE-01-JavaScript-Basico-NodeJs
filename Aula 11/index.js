b404ab6854d932148fdf5843b2588a4c153f989b
Leo Ruiz <ruizruizleo@hotmail.com>
2023-01-02 02:26:16 -0300

    Aula 11 -  Variaveis primitivas

---
+++ Aula 11/index.js
@@ -0,0 +1,15 @@
+console.log("Hello World");
+
+var numero = 10;
+
+var nome = "Leonardo"
+var idade = 25
+var sexo = "M"
+
+var teste = true
+
+teste = "outra info"
+
+console.log(nome);
+console.log(idade);
+console.log(sexo);
