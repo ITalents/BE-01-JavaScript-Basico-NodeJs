f05a9cbf3eaee26e49d539eea9864e9b3e8b4da3
Leo Ruiz <ruizruizleo@hotmail.com>
2023-01-02 23:13:52 -0300

    Aula 12 - Arrays

---
+++ Aula 12/index.js
@@ -0,0 +1,18 @@
+var nome = "Leonardo";
+var idade = 15;
+var sexo = "M";
+var altura = 1.72;
+var peso = 73.2;
+
+var nomes = ["leonardo","Lucas","João"];
+
+console.log(nomes);
+
+nomes.push("Thais");
+
+console.log(nomes[3]);
+
+nomes.push(12);
+
+console.log(nomes);
+console.log(nomes[4]);
