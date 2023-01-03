a91aac6362f7b08722c8d551bdb3a759c97666c6
Leo Ruiz <ruizruizleo@hotmail.com>
2023-01-03 00:10:41 -0300

    Aula 14 - Objetos

---
+++ Aula 14/index.js
@@ -0,0 +1,22 @@
+var pessoa = new Object();
+var listaPessoas = [];
+
+pessoa["nome"] = "Leonardo";
+pessoa["idade"] = 25;
+pessoa["sexo"] = "M";
+pessoa["peso"] = 105;
+pessoa["altura"] = 1.88;
+
+listaPessoas.push(pessoa);
+
+var pessoa = new Object();
+pessoa["nome"] = "Lucas";
+pessoa["idade"] = 21;
+pessoa["sexo"] = "M";
+pessoa["peso"] = 75;
+pessoa["altura"] = 1.78;
+
+listaPessoas.push(pessoa);
+pessoa["idade"] = 23;
+
+console.log(listaPessoas);
