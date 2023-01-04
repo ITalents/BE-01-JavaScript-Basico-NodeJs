09599781d035e2485a21d9a3b19dfc6f3c178a58
Leo Ruiz <ruizruizleo@hotmail.com>
2023-01-04 00:20:49 -0300

    Aula 16 - Estrutura de decisão

---
+++ Aula 16/index.js
@@ -0,0 +1,22 @@
+if (9 > 10) {
+    console.log("primeiro if");
+}else{
+    console.log("primeiro else");
+}
+
+if(9 > 6){
+    console.log("segundo if");
+}
+
+
+if(9 >= 3){
+    console.log("terceiro if");
+}
+
+if(9 != 9){
+    console.log("diferente de 9");
+}else if( 5 <= 8){
+    console.log("5 é menor ou igual a 8");
+}else if(9 < 4){
+    console.log("9 é menor q 4");
+}
