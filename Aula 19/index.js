06ecd8bd8af774db87a1d2339484fa942579e99b
Leo Ruiz <ruizruizleo@hotmail.com>
2023-01-04 21:53:57 -0300

    Aula 19 - update do projeto

--- Aula 19/index.js
+++ Aula 19/index.js
@@ -2,8 +2,9 @@ const prompt = require("prompt-sync")();
 
 let notas = [];
 
-function capturaNotas(){
-    const numNotas = +prompt("Quantas notas deseja digitar? ");
+const numNotas = +prompt("Quantas notas deseja digitar? ");
+
+function capturarNotas(){
     for (let i = 0; i < numNotas; i++) {
         notas.push(+prompt(`${i+1} - Digite a nota: `)); 
     }
@@ -11,10 +12,37 @@ function capturaNotas(){
     let media = 0;
     notas.forEach((value) => {
         media += value;
-    })
+    });
     media =  media / notas.length;
 
     return media;
 }
 
-console.log(capturaNotas());
+const media1 = capturarNotas();
+
+if(media1 >= 6 ){
+    console.log("");
+    console.log(`Aluno aprovado com média: ${media1}`);
+    console.log("Parabenssss!!!!!");
+    console.log("");
+}else{
+    console.log("");
+    console.log(`Aluno não aprovado, média: ${media1}`);
+    console.log("Encaminhado a recuperação!!!");
+    console.log("");
+    
+    notas = []
+    const media2 = capturarNotas();
+
+    if(media2 >= 6 ){
+        console.log("");
+        console.log(`Aluno aprovado com média: ${media2}`);
+        console.log("Parabenssss!!!!!");
+        console.log("");
+    }else{
+        console.log("");
+        console.log(`Aluno não aprovado, média: ${media2}`);
+        console.log("Aluno retido por nota!!!");
+        console.log("");
+    }
+}
